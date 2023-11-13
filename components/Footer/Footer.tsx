e1777c5925a699acb8e2ca94731bb167ae9acce2
sandygudie <goodnewssandy@gmail.com>
2023-11-13 17:40:31 +0100

    added fixes

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { IoLogoYoutube } from 'react-icons/io'
-import { AiOutlineInstagram, AiOutlineTwitter } from 'react-icons/ai'
+import { AiOutlineInstagram } from 'react-icons/ai'
 import { BiLogoFacebook } from 'react-icons/bi'
 import { FaXTwitter } from 'react-icons/fa6'
 
@@ -28,7 +28,7 @@ const Footer = () => {
       <div className="p-10 flex flex-col lg:flex-row gap-8 lg:gap-6 justify-center items-start lg:items-center text-white bg-[#333333]">
         <Image src="/assets/images/logo.png" width="50" height="50" alt="logo" />
         <div className="text-sm text-left">
-          <p>2016-2023</p>
+          <p>&copy;{new Date().getFullYear()}</p>
           <p>Izesan. All right reserved.</p>
           <p>A division of Otaigbe Groups</p>
         </div>
@@ -51,23 +51,26 @@ const Footer = () => {
           <Link className="block hover:text-yellow" href="/">
             Register
           </Link>
-          <Link className=" block hover:text-yellow" href="/">
+          <Link className=" block hover:text-yellow" href="/login">
             Sign In
           </Link>
         </div>
         <hr className="border-white rotate-90 w-10 hidden lg:block" />
         <div className="flex gap-4 justify-center  items-center ">
           {' '}
-          <Link className="text-xl hover:text-yellow" href="#">
+          <Link className="text-xl hover:text-yellow" href="https://www.youtube.com/@izesan392">
             <IoLogoYoutube />
           </Link>
-          <Link className="text-xl  hover:text-yellow" href="#">
+          <Link className="text-xl  hover:text-yellow" href="https://www.instagram.com/lzesan/">
             <AiOutlineInstagram />
           </Link>
-          <Link className="text-xl  hover:text-yellow" href="#">
+          <Link className="text-xl  hover:text-yellow" href="https://twitter.com/lzesan">
             <FaXTwitter />
           </Link>
-          <Link className="text-xl  hover:text-yellow" href="#">
+          <Link
+            className="text-xl  hover:text-yellow"
+            href="https://www.facebook.com/izesanspeakesan/"
+          >
             <BiLogoFacebook />
           </Link>
         </div>
